909cd41f07e9f3e30e893a324de0dda4687d4d60
Spencer Dixon <spencercdixon@gmail.com>
2017-04-27 00:31:26 -0400

    Adds uglify plugin to production frontend build

--- frontend/webpack/webpack.prod.config.js
+++ frontend/webpack/webpack.prod.config.js
@@ -1,11 +1,13 @@
 const webpack = require('webpack');
 const common = require('./webpack.common.config.js');
+
 // const Visualizer = require('webpack-visualizer-plugin');
 
 common.plugins.push(
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify('production'),
-  })
+  }),
+  new webpack.optimize.UglifyJSPlugin()
   // new Visualizer()
 );
 
